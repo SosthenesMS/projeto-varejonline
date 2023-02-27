2461508117c02ce1fd51336606a30218821da706
SosthenesMS <sosthenesms@yahoo.com.br>
2023-02-27 02:58:21 -0300

    Correções das camadas produtoRepository e MovimentacoesRepository

--- server/src/main/java/br/comvarejonline/projetoinicial/repository/MovimentacoesEstoqueRepository.java
+++ server/src/main/java/br/comvarejonline/projetoinicial/repository/MovimentacoesEstoqueRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.comvarejonline.projetoinicial.model.MovimentacoesEstoque;
 
 public interface MovimentacoesEstoqueRepository extends JpaRepository<MovimentacoesEstoque, Integer> {
-
+    
     Page<MovimentacoesEstoque> findAll(Pageable pageable);
 
 }
